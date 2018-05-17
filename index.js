28490081bd07975440a6857053f87410ecd342a2
Luigi Mannoni <luigimannoni@gmail.com>
2018-05-17 12:27:25 +0100

    Updated to Node6+ ES6 syntax, switched to ESLint, updated deprecated MongoDB dependency

--- index.js
+++ index.js
@@ -1,46 +1,32 @@
-var Binary = require('mongodb').Binary
+const { Binary } = require('mongodb');
 
-module.exports =
-{ binToJUUIDString: function (binary) {
-    var hex = binary.buffer.toString('hex')
-      , msb = hex.substr(0, 16)
-      , lsb = hex.substr(16, 16)
+const hexToBase64 = hex => Buffer.from(hex, 'hex').toString('base64');
+const significantBit = msb => `${msb.substr(14, 2)}${msb.substr(12, 2)}${msb.substr(10, 2)}${msb.substr(8, 2)}${msb.substr(6, 2)}${msb.substr(4, 2)}${msb.substr(2, 2)}${msb.substr(0, 2)}`;
 
-    msb = significantBit(msb)
+module.exports = {
+  binToJUUIDString: (binary) => {
+    let hex = binary.buffer.toString('hex');
+    let msb = hex.substr(0, 16);
+    let lsb = hex.substr(16, 16);
 
-    lsb = significantBit(lsb)
+    msb = significantBit(msb);
+    lsb = significantBit(lsb);
+    hex = msb + lsb;
 
-    hex = msb + lsb
+    const uuid = `${hex.substr(0, 8)}-${hex.substr(8, 4)}-${hex.substr(12, 4)}-${hex.substr(16, 4)}-${hex.substr(20, 12)}`;
+    return uuid;
+  },
+  juuidStringToBin: (juuid) => {
+    let hex = juuid.replace(/[{}-]/g, '');
+    let msb = hex.substr(0, 16);
+    let lsb = hex.substr(16, 16);
 
-    var uuid = hex.substr(0, 8) + '-' + hex.substr(8, 4) + '-' + hex.substr(12, 4)
-      + '-' + hex.substr(16, 4) + '-' + hex.substr(20, 12)
+    msb = significantBit(msb);
+    lsb = significantBit(lsb);
 
-    return uuid
-  }
+    hex = msb + lsb;
 
-, juuidStringToBin: function (juuid) {
-    var hex = juuid.replace(/[{}-]/g, '') // remove extra characters
-      , msb = hex.substr(0, 16)
-      , lsb = hex.substr(16, 16)
-
-    msb = significantBit(msb)
-    lsb = significantBit(lsb)
-
-    hex = msb + lsb
-
-    return new Binary(new Buffer(hexToBase64(hex), 'base64'), 3)
-  }
-
-, hexToBase64: hexToBase64
-}
-
-function hexToBase64(hex) {
-  return new Buffer(hex, 'hex').toString('base64')
-}
-
-function significantBit(msb) {
-  msb = msb.substr(14, 2) + msb.substr(12, 2) + msb.substr(10, 2) + msb.substr(8, 2)
-    + msb.substr(6, 2) + msb.substr(4, 2) + msb.substr(2, 2) + msb.substr(0, 2)
-
-  return msb
-}
+    return new Binary(Buffer.from(hexToBase64(hex), 'base64'), 3);
+  },
+  hexToBase64,
+};
