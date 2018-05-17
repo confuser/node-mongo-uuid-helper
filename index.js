272fa3b73248e576447440ac92bba31f1f8ff799
James Mortemore <jamesmortemore@gmail.com>
2018-05-17 14:26:21 +0100

    Updated to use ES6, removed old dev dependencies

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-var Binary = require('mongodb').Binary
+const { Binary } = require('mongodb')
 
-module.exports =
-{ binToJUUIDString: function (binary) {
-    var hex = binary.buffer.toString('hex')
-      , msb = hex.substr(0, 16)
-      , lsb = hex.substr(16, 16)
+module.exports = {
+  binToJUUIDString: function (binary) {
+    let hex = binary.buffer.toString('hex')
+    let msb = hex.substr(0, 16)
+    let lsb = hex.substr(16, 16)
 
     msb = significantBit(msb)
-
     lsb = significantBit(lsb)
-
     hex = msb + lsb
 
-    var uuid = hex.substr(0, 8) + '-' + hex.substr(8, 4) + '-' + hex.substr(12, 4)
-      + '-' + hex.substr(16, 4) + '-' + hex.substr(20, 12)
+    const uuid = hex.substr(0, 8) + '-' + hex.substr(8, 4) + '-' + hex.substr(12, 4) +
+        '-' + hex.substr(16, 4) + '-' + hex.substr(20, 12)
 
     return uuid
-  }
-
-, juuidStringToBin: function (juuid) {
-    var hex = juuid.replace(/[{}-]/g, '') // remove extra characters
-      , msb = hex.substr(0, 16)
-      , lsb = hex.substr(16, 16)
+  },
+  juuidStringToBin: function (juuid) {
+    let hex = juuid.replace(/[{}-]/g, '') // remove extra characters
+    let msb = hex.substr(0, 16)
+    let lsb = hex.substr(16, 16)
 
     msb = significantBit(msb)
     lsb = significantBit(lsb)
 
     hex = msb + lsb
 
-    return new Binary(new Buffer(hexToBase64(hex), 'base64'), 3)
-  }
-
-, hexToBase64: hexToBase64
+    return new Binary(Buffer.from(hexToBase64(hex), 'base64'), 3)
+  },
+  hexToBase64
 }
 
-function hexToBase64(hex) {
-  return new Buffer(hex, 'hex').toString('base64')
+function hexToBase64 (hex) {
+  return Buffer.from(hex, 'hex').toString('base64')
 }
 
-function significantBit(msb) {
-  msb = msb.substr(14, 2) + msb.substr(12, 2) + msb.substr(10, 2) + msb.substr(8, 2)
-    + msb.substr(6, 2) + msb.substr(4, 2) + msb.substr(2, 2) + msb.substr(0, 2)
+function significantBit (msb) {
+  msb = msb.substr(14, 2) + msb.substr(12, 2) + msb.substr(10, 2) + msb.substr(8, 2) +
+    msb.substr(6, 2) + msb.substr(4, 2) + msb.substr(2, 2) + msb.substr(0, 2)
 
   return msb
 }
--- test/mongo-uuid-helper.test.js
+++ test/mongo-uuid-helper.test.js
@@ -1,18 +1,13 @@
-var uuidHelper = require('../')
-
-require('should')
+const assert = require('assert')
+const uuidHelper = require('../')
 
 describe('Mongo UUID Helper', function () {
-
   describe('#binToJUUIDString', function () {
-
     it('should convert binary to a juuid string', function () {
-      var juuidStr = 'ae51c849-3f2a-4a37-986d-55ed5b02307f'
-        , binary = uuidHelper.juuidStringToBin(juuidStr)
+      const juuidStr = 'ae51c849-3f2a-4a37-986d-55ed5b02307f'
+      const binary = uuidHelper.juuidStringToBin(juuidStr)
 
-      uuidHelper.binToJUUIDString(binary).should.equal(juuidStr)
+      assert.strictEqual(uuidHelper.binToJUUIDString(binary), juuidStr)
     })
-
   })
-
 })
